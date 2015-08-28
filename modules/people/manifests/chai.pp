b4aefdb5e8818ef2ca40d822c4a3eac9fd456f0a
chai <chaihongnge@gmail.com>
2015-08-28 21:51:54 +0900

    Add googledrive

--- modules/people/manifests/chai.pp
+++ modules/people/manifests/chai.pp
@@ -21,18 +21,6 @@ class people::chai {
 
   notify { 'class people::chai declared': }
 
-  # Changes the default shell to the zsh version we get from Homebrew
-  # Uses the osx_chsh type out of boxen/puppet-osx
-  osx_chsh { $::luser:
-    shell   => '/opt/boxen/homebrew/bin/zsh',
-    require => Package['zsh'],
-  }
-
-  file_line { 'add zsh to /etc/shells':
-    path    => '/etc/shells',
-    line    => "${boxen::config::homebrewdir}/bin/zsh",
-    require => Package['zsh'],
-  }
   ##################################
   ## Facter, Puppet, and Envpuppet##
   ##################################
