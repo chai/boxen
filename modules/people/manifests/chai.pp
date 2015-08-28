46e7025ee1afe79f0b692a3587ea9a20e7f9bdb2
chai <chaihongnge@gmail.com>
2015-08-29 01:16:08 +0900

    Add osx

--- modules/people/manifests/chai.pp
+++ modules/people/manifests/chai.pp
@@ -9,6 +9,16 @@ include android::tools
 include android::platform_tools
 include android::studio
 
+include osx::global::expand_print_dialog  #- expand the print dialog by default
+include osx::global::expand_save_dialog   #- expand the save dialog by default
+include osx::global::tap_to_click         #- enables tap to click
+include osx::finder::unhide_library       #- unsets the hidden flag on ~/Library
+include osx::finder::show_hidden_files
+include osx::no_network_dsstores          #- disable creation of .DS_Store files on network shares
+include osx::software_update              #- download and install software updates
+include osx::dock::icon_size              #-set the size to 36
+
+
 
 
 class people::chai {
@@ -42,6 +52,9 @@ class people::chai {
 
   android::build_tools{ '22':
   }
+
+  osx::recovery_message { 'If this Mac is found, please call 090-3140-0282': }
+
 
 
 
--- modules/people/manifests/chai/osx.pp
+++ modules/people/manifests/chai/osx.pp
@@ -1,22 +1,7 @@
 
-include osx::global::expand_print_dialog  #- expand the print dialog by default
-include osx::global::expand_save_dialog   #- expand the save dialog by default
-include osx::global::tap_to_click         #- enables tap to click
-include osx::finder::unhide_library       #- unsets the hidden flag on ~/Library
-include osx::finder::show_hidden_files
-include osx::no_network_dsstores          #- disable creation of .DS_Store files on network shares
-include osx::software_update              #- download and install software updates
-
-include osx::dock::icon_size              #-set the size to 36
-
-
 class people::chai::osx{
 
-
-
-osx::recovery_message { 'If this Mac is found, please call 090-3140-0282': }
-
-
+  notify { 'class people::chai::osx declared': }
 
 
 }
